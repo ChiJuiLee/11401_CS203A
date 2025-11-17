db5b90f05907cd1677d8975be04557cfcb3425ee
ChiJuiLee <ready950110@gmail.com>
2025-11-17 21:15:18 +0800

    hash cpp change

--- Assingment/AssingmentIV/CPP/hash_fn.cpp
+++ Assingment/AssingmentIV/CPP/hash_fn.cpp
@@ -1,25 +1,29 @@
 /*
    ========================================
-   hash_fn.cpp — implement your hash functions
+   hash_fn.cpp ??? implement your hash functions
    ========================================
 
    Description:
     This file contains the hash functions for integer and string keys.
 
    Development History:
-    - 2025/11/11: Initial implementation
+    - 2025/11/17: Initial implementation
 
-   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+   Developer: CHIJUILEE <ready950110@gmail.com>
  */
 #include <string>
-
+#include <cmath>
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
-    return key % m;  // basic division method
+    const double g = 0.618; //  1/Golden Ratio. golden ration:(sqrt(5)-1)/2
+    double frac = (key * g) - floor(key * g);
+    return static_cast<int>(floor(m * frac));
 }
 
 int myHashString(const std::string& str, int m) {
-    unsigned long hash = 0;
-    // TODO: replace with your own design
+    unsigned long hash = 5381;
+    for(char c : str)
+    {
+        hash = ((hash << 5) + hash) + c;
+    }
     return static_cast<int>(hash % m);  // basic division method
 }
--- Assingment/AssingmentIV/CPP/main.cpp
+++ Assingment/AssingmentIV/CPP/main.cpp
@@ -9,9 +9,9 @@
    defined in "hash_fn.cpp".
 
    Development History:
-    - 2025/11/11: Initial implementation
+    - 2025/11/17: Initial implementation
 
-   Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
+   Developer: CHIJUILEE <ready950110@gmail.com>
  */
 
 #include <iostream>
